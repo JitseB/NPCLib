4ed0d4998469427c2096ac3341c9e736b68615dd
MrMicky <git@mrmicky.fr>
2020-02-19 14:58:18 +0100

    Add Minecraft 1.15 support

---
+++ nms/v1_15_R1/src/main/java/net/jitse/npclib/nms/v1_15_R1/NPC_v1_15_R1.java
@@ -0,0 +1,110 @@
+package net.jitse.npclib.nms.v1_15_R1;
+
+import net.jitse.npclib.NPCLib;
+import net.jitse.npclib.api.state.NPCSlot;
+import net.jitse.npclib.hologram.Hologram;
+import net.jitse.npclib.internal.MinecraftVersion;
+import net.jitse.npclib.internal.NPCBase;
+import net.jitse.npclib.nms.v1_15_R1.packets.*;
+import net.minecraft.server.v1_15_R1.*;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_15_R1.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
+/**
+ * @author Jitse Boonstra
+ */
+public class NPC_v1_15_R1 extends NPCBase {
+
+    private PacketPlayOutNamedEntitySpawn packetPlayOutNamedEntitySpawn;
+    private PacketPlayOutScoreboardTeam packetPlayOutScoreboardTeamRegister;
+    private PacketPlayOutPlayerInfo packetPlayOutPlayerInfoAdd, packetPlayOutPlayerInfoRemove;
+    private PacketPlayOutEntityHeadRotation packetPlayOutEntityHeadRotation;
+    private PacketPlayOutEntityDestroy packetPlayOutEntityDestroy;
+
+    public NPC_v1_15_R1(NPCLib instance, List<String> lines) {
+        super(instance, lines);
+    }
+
+    @Override
+    public void createPackets() {
+        this.hologram = new Hologram(MinecraftVersion.V1_15_R1, location.clone().add(0, 0.5, 0), text);
+
+        PacketPlayOutPlayerInfoWrapper packetPlayOutPlayerInfoWrapper = new PacketPlayOutPlayerInfoWrapper();
+
+        // Packets for spawning the NPC:
+        this.packetPlayOutScoreboardTeamRegister = new PacketPlayOutScoreboardTeamWrapper()
+                .createRegisterTeam(name); // First packet to send.
+
+        this.packetPlayOutPlayerInfoAdd = packetPlayOutPlayerInfoWrapper
+                .create(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, gameProfile, name); // Second packet to send.
+
+        this.packetPlayOutNamedEntitySpawn = new PacketPlayOutNamedEntitySpawnWrapper()
+                .create(uuid, location, entityId); // Third packet to send.
+
+        this.packetPlayOutEntityHeadRotation = new PacketPlayOutEntityHeadRotationWrapper()
+                .create(location, entityId); // Fourth packet to send.
+
+        this.packetPlayOutPlayerInfoRemove = packetPlayOutPlayerInfoWrapper
+                .create(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.REMOVE_PLAYER, gameProfile, name); // Fifth packet to send (delayed).
+
+        // Packet for destroying the NPC:
+        this.packetPlayOutEntityDestroy = new PacketPlayOutEntityDestroy(entityId); // First packet to send.
+    }
+
+    @Override
+    public void onLogout(Player player) {
+        super.onLogout(player);
+        hasTeamRegistered.remove(player.getUniqueId());
+    }
+
+    @Override
+    public void sendShowPackets(Player player) {
+        PlayerConnection playerConnection = ((CraftPlayer) player).getHandle().playerConnection;
+
+        if (hasTeamRegistered.add(player.getUniqueId()))
+            playerConnection.sendPacket(packetPlayOutScoreboardTeamRegister);
+        playerConnection.sendPacket(packetPlayOutPlayerInfoAdd);
+        playerConnection.sendPacket(packetPlayOutNamedEntitySpawn);
+        playerConnection.sendPacket(packetPlayOutEntityHeadRotation);
+        sendMetadataPacket(player);
+
+        hologram.show(player);
+
+        Bukkit.getScheduler().runTaskLater(instance.getPlugin(), () ->
+                playerConnection.sendPacket(packetPlayOutPlayerInfoRemove), 50);
+    }
+
+    @Override
+    public void sendHidePackets(Player player) {
+        PlayerConnection playerConnection = ((CraftPlayer) player).getHandle().playerConnection;
+
+        playerConnection.sendPacket(packetPlayOutEntityDestroy);
+        playerConnection.sendPacket(packetPlayOutPlayerInfoRemove);
+
+        hologram.hide(player);
+    }
+
+    @Override
+    public void sendMetadataPacket(Player player) {
+        PlayerConnection playerConnection = ((CraftPlayer) player).getHandle().playerConnection;
+        PacketPlayOutEntityMetadata packet = new PacketPlayOutEntityMetadataWrapper().create(activeStates, entityId);
+
+        playerConnection.sendPacket(packet);
+    }
+
+    @Override
+    public void sendEquipmentPacket(Player player, NPCSlot slot, boolean auto) {
+        PlayerConnection playerConnection = ((CraftPlayer) player).getHandle().playerConnection;
+
+        EnumItemSlot nmsSlot = slot.getNmsEnum(EnumItemSlot.class);
+        ItemStack item = getItem(slot);
+
+        PacketPlayOutEntityEquipment packet = new PacketPlayOutEntityEquipment(entityId, nmsSlot, CraftItemStack.asNMSCopy(item));
+        playerConnection.sendPacket(packet);
+    }
+}
